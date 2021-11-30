bcdb953685cedf20e43273de60160fb0efdfe194
4ever20 <kirbyderby2000@gmail.com>
2021-11-29 19:38:11 -0800

    - Created primary and secondary weapon scripts inheritting the base weapon class. All primary weapon scripts created should inherit the primary weapon script, as all secondary weapon scripts should inherit the secondary weapon script. Individual weapon scripts created may provide their own overridden implementation from their base classes.

---
+++ Assets/Scripts/Problem 2 Scripts/PrimaryWeapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for primary weapons. All primary weapons need to inherit from this base class.
+/// </summary>
+public abstract class PrimaryWeapon : Weapon
+{
+
+}
---
+++ Assets/Scripts/Problem 2 Scripts/SecondaryWeapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for secondary weapons. All secondary weapons need to inherit from this base class.
+/// </summary>
+public abstract class SecondaryWeapon : Weapon
+{
+
+}
